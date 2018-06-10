b3e75821b5e010877c5cabf28a13c18be7b5c2fa
Barzarrhey_mac <pcmosfet@outlook.com>
2018-06-10 22:40:32 +0800

    rebuild the welcome but not finished...

--- crawfish.py
+++ crawfish.py
@@ -3,8 +3,12 @@
 # Created by barzarrhey on 2018/6/10
 
 from qtcv import Qtcv
+from start import drawSnake
+import sys
 
 def main():
+    drawsnake = drawSnake()
+    drawsnake.draw()
     Qtcv.run()
 
 main()
--- start.py
+++ start.py
@@ -4,20 +4,23 @@
 import turtle
 import sys
 import turtle
+import time
 
 class drawSnake():
     def __init__(self):
-        self.rad = 40
+        self.rad = 20
         self.angle = 80
         self.len = 6
         self.colors = ["red", "orange", "yellow", "green", "cyan", "blue"]
-        self.pythonSize = 30
+        self.pythonSize = 15
         self.neckRad = self.pythonSize / 2
 
     def draw(self):
-        turtle.setup(1300, 800, 0, 0)
+        turtle.setup(600, 400)
+        turtle.hideturtle()
+        turtle.speed(0)
         turtle.penup()
-        turtle.goto(-350, 0)
+        turtle.goto(-200, 50)
         turtle.pendown()
         turtle.pensize(self.pythonSize)
         turtle.seth(-40)
@@ -31,7 +34,31 @@ class drawSnake():
         turtle.fd(self.rad)
         turtle.circle(self.neckRad + 1, 180)
         turtle.fd(self.rad * 2 / 3)
+        turtle.penup()
+        turtle.goto(95,-60)
+        turtle.pendown()
+        turtle.write("让我们吃着小龙虾", align="center", font=("Courier", 20, "pink"))
+        turtle.penup()
+        turtle.goto(95, -80)
+        turtle.pendown()
+        turtle.write("看着屏幕", align="center", font=("Courier", 20, "pink"))
+        turtle.penup()
+        turtle.goto(95, -100)
+        turtle.pendown()
+        turtle.write("享受这大千世界与开源不求回报的礼物", align="center", font=("Courier", 20, "pink"))
+        turtle.penup()
+        turtle.goto(95, -120)
+        turtle.pendown()
+        turtle.write("Forever don't forget the day\nthat you konw", align="center", font=("Courier", 20, "pink"))
+        turtle.penup()
+        turtle.goto(95, -140)
+        turtle.pendown()
+        turtle.write("that someone told you excitedly needn't rewhell", align="center", font=("Courier", 20, "pink"))
+
+
+        # turtle.write(s, font=(“font-name”, font_size, ”font_type”))
+        time.sleep(5)
 
 if __name__ == '__main__':
     drawsnake = drawSnake()
-    sys.exit(drawsnake.draw())
+    drawsnake.draw()
