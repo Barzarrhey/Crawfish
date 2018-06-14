0ff633313d9c0004d585460b86c0db00483f016d
Barzarrhey_mac <pcmosfet@outlook.com>
2018-06-14 17:11:04 +0800

    rebuild the structure of the folder

---
+++ src/crawfish.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by barzarrhey on 2018/6/10
+
+from qtcv import Qtcv
+from start import drawSnake
+import os
+
+def main():
+    # drawsnake = drawSnake()
+    # drawsnake.draw()
+    # Qtcv.run()
+    cmd1 = "python start.py"
+    cmd2 = "python qtcv.py"
+    os.system(cmd1)
+    os.system(cmd2)
+
+
+main()
